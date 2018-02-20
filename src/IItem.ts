6b48e5ae87c74268926a9d561fdbf31e29842a24
kuba <kanatkubash@gmail.com>
2018-02-20 15:02:31 +0600

    Comments for IItem and Item

--- src/IItem.ts
+++ src/IItem.ts
@@ -1,6 +1,21 @@
+/**
+ * Item interface for items in basket
+ */
 export default interface IItem {
+	/**
+	 * Gets name of item
+	 */
 	name?: any;
+	/**
+	 * Gets description of item
+	 */
 	description?: any;
+	/**
+	 * Gets quantity of give item
+	 */
 	quantity?: any;
+	/**
+	 * Gets price per item
+	 */
 	price?: any;
 }
--- src/Item.ts
+++ src/Item.ts
@@ -3,39 +3,61 @@ import { initialize as initializeHelper } from './helpers'
 interface IObject {
 	[key: string]: any;
 }
+/**
+ * Item class for representing items in basket
+ */
 export default class Item implements IItem {
 	protected _parameters: IObject = {};
+	/**
+	 * Create new Item
+	 * @param parameters parameters to initialize from
+	 */
 	constructor(parameters: IObject = {}) {
 		this.initialize(parameters);
 	}
+	/**
+	 * Initalize Item with given parameters
+	 * @param parameters parameters
+	 */
 	initialize(parameters: IObject = {}): this {
 		initializeHelper(this, parameters);
 		return this;
 	}
+	/**
+	 * Get all parameters of Item
+	 */
 	get parameters(): IObject {
 		return this._parameters;
 	}
+	/** @inheritdoc */
 	get name() {
 		return this.parameters['name'];
 	}
+	/** @inheritdoc */
 	set name(value: string) {
 		this.parameters['name'] = value;
 	}
+	/** @inheritdoc */
 	get description() {
 		return this.parameters['description'];
 	}
+	/** @inheritdoc */
 	set description(value: string) {
 		this.parameters['description'] = value;
 	}
+	/** @inheritdoc */
 	get quantity() {
 		return this.parameters['quantity'];
 	}
+	/** @inheritdoc */
 	set quantity(value: number) {
 		this.parameters['quantity'] = value;
 	}
+	/** @inheritdoc */
 	get price() {
 		return this.parameters['price'];
 	}
+	/** @inheritdoc */
 	set price(value: number) {
 		this.parameters['price'] = value;
 	}
