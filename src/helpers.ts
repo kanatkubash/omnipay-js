4e123e6b1c1af9def26a2dcdc5229e921b026fa8
kuba <kanatkubash@gmail.com>
2018-02-21 13:48:06 +0600

    Added luhn validation to helper Added helper tests

--- src/helpers.ts
+++ src/helpers.ts
@@ -9,3 +9,15 @@ export function initialize(object: any, parameters: IObject = {}) {
 			object[key] = parameters[key];
 	}
 }
+export function validateLuhn(number: string): boolean {
+	if (!number) return true;
+	var str = Array.from(number)
+		.reverse()
+		.map(a => Number(a))
+		.reduce((acc, val, i) => acc + (i % 2 ? (val * 2) : val), '');
+	return Array.from(str)
+		.reduce((acc, val) => acc + Number(val), 0) % 10 === 0;
+}
+/// Instead of getting gateway name from class , 
+/// we would be using decorators on impl classes
+export var getGatewayShortName = () => { }
---
+++ test/helperTest.spec.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { validateLuhn, initialize } from '../src/helpers';
+import * as sinon from 'sinon';
+import { spy } from 'sinon';
+import CreditCard from '../src/CreditCard';
+chai.use(require('sinon-chai'));
+require('mocha');
+describe('Helper', () => {
+	describe('Luhn validator', () => {
+		it('should validate luhn', () => {
+			var res = validateLuhn('4111111111111111');
+			expect(res).to.equal(true);
+			var res = validateLuhn('4111111111111110');
+			expect(res).to.equal(false);
+			var res = validateLuhn('378282246310005');
+			expect(res).to.equal(true);
+			var res = validateLuhn('5555555555554444');
+			expect(res).to.equal(true);
+			var res = validateLuhn('378282246310005');
+			expect(res).to.equal(true);
+			var res = validateLuhn('378282246310006');
+			expect(res).to.equal(false);
+		})
+		it('should return true to null', () => {
+			var res = validateLuhn(null);
+			expect(res).to.equal(true);
+		})
+	})
+	describe('Initalizer', () => {
+		it('should ignore null', () => {
+			initialize({}, null);
+		})
+		it('should call setters', () => {
+			var nameSpy = sinon.spy(), numberSpy = sinon.spy();
+			var card = sinon.createStubInstance(CreditCard);
+			sinon.stub(card, 'number').set(numberSpy);
+			sinon.stub(card, 'name').set(nameSpy);
+			initialize(card, { number: '1234', name: 'Zhake' });
+			expect(numberSpy).to.have.been.calledWith('1234').calledOnce;
+			expect(nameSpy).to.have.been.calledWith('Zhake').calledOnce;
+		})
+		it('should ignore invalid parameters', () => {
+			var params: any = [];
+			class TestClass {
+				set name(value: string) { }
+			}
+			var testClass = new TestClass;
+			var nameSpy = sinon.spy(testClass, 'name', ['set']);
+			initialize(testClass, { name: 'name', extra: 'ba' });
+			expect(nameSpy.set).to.have.been.calledWith('name').calledOnce;
+		})
+	})
+})
